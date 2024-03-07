130b4827aeab8bc0273617a18c28c0b28358185c
Kevil-Gandhi <156393907+Kevil-Gandhi@users.noreply.github.com>
2024-03-07 19:22:37 +0530

    JS file added.

---
+++ script.js
@@ -0,0 +1,42 @@
+let input = document.getElementById("inputBox");
+let numbers = document.querySelectorAll(".number");
+let operator = document.querySelectorAll(".operator");
+let ac = document.querySelector(".ac");
+let del = document.querySelector(".del");
+
+let string = "";
+
+console.log(input);
+console.log(numbers);
+console.log(operator);
+
+numbers.forEach(button => {
+
+    button.addEventListener("click",()=>{
+        string += button.innerHTML;
+        input.value = string;
+    })
+})
+
+operator.forEach(button => {
+
+    button.addEventListener("click",()=>{
+        string += button.innerHTML;
+        input.value = string;
+    })
+})
+
+ac.addEventListener("click",()=>{
+    string = "";
+    input.value = string;
+})
+
+del.addEventListener("click",()=>{
+    string = string.substring(0,string.length-1);
+    input.value = string;
+})
+
+let Evaluate = () =>{
+    string = eval(string);
+    input.value = string;
+};
